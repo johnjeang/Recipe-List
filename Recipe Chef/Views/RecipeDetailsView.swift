62140abaf02fcad312af5e7d633f2395e638d87d
johnjeang <johnjeang@gmail.com>
2021-03-24 09:31:39 -0500

    Modify json data to account for serving size functionality

--- Recipe Chef/Views/RecipeDetailsView.swift
+++ Recipe Chef/Views/RecipeDetailsView.swift
@@ -27,8 +27,8 @@ struct RecipeDetailsView: View {
                     Text("Ingredients")
                         .font(.headline)
                         .padding(.bottom, 5)
-                    ForEach(recipe.ingredients, id: \.self){ingredient in
-                        Text("• " + ingredient)
+                    ForEach(recipe.ingredients){ingredient in
+                        Text("• " + ingredient.name)
                             .padding(.bottom, 1)
                     }
                 }
